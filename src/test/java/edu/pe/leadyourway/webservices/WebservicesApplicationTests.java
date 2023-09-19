fa96b65d6925bd1f569b3028d22fcf90fdc93e90
LenninHuaman <u20201b975@upc.edu.pe>
2023-09-18 20:21:02 -0500

    Created app's file structure

---
+++ src/test/java/edu/pe/leadyourway/webservices/WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.leadyourway.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
